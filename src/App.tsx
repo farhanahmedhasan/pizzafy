161aaeb35ccc8543362455b162c3e1c8bc52eebb
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-03-22 16:08:41 +0600

    added tailwind and prettier rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 function App() {
-  return <h1 className="text-3xl font-semibold">Yo Vite</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold">Yo Vite</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam qui praesentium voluptatibus voluptas, eligendi
+        inventore. Culpa placeat ullam corrupti, repellendus illo alias distinctio molestiae obcaecati libero inventore
+        ut, quibusdam rem facilis asperiores quo, dicta corporis natus odit vitae reprehenderit delectus iusto. Odit
+        aliquid, quidem, et omnis quam commodi dolor nihil illum optio architecto, odio provident? Incidunt officiis quo
+        repudiandae fugit suscipit alias ipsa quia porro ipsam tenetur quam obcaecati quis, voluptatem libero quisquam
+        ex dolorem tempore. Voluptatibus aut, qui aliquid accusantium libero quam exercitationem laudantium! Esse sunt
+        laudantium natus dignissimos nostrum quis eius quisquam qui doloremque, iure pariatur exercitationem voluptate
+        saepe numquam atque officiis dolorum minima eos officia quasi nesciunt earum consequatur nemo optio? Aliquam
+        veniam explicabo unde provident repudiandae illo non, reiciendis voluptas, eos ad necessitatibus temporibus
+        corrupti odit fuga earum soluta cupiditate excepturi rem repellendus blanditiis quo aperiam nobis libero. Cumque
+        error exercitationem rem nulla consectetur? Enim vel, laboriosam exercitationem quas maxime ipsum asperiores
+        expedita debitis. Odio modi necessitatibus placeat esse eum minus omnis animi vel cum, ullam pariatur voluptatum
+        aliquam voluptatem possimus. Similique, animi repellat! Dolores quaerat iusto perspiciatis recusandae corporis
+        odit nemo animi, exercitationem labore cumque, obcaecati ipsam, quis corrupti excepturi eveniet voluptate
+        inventore illo fugiat!
+      </p>
+    </div>
+  )
 }
 
-export default App;
+export default App
