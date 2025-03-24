070038a30e2d9f2c46e317b0ca52b627f38a5e9f
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-03-24 22:50:02 +0600

    added component/ui to have reuseable components hooks folder to have custom hook

---
+++ src/pages/errorpage/Error404Page.tsx
@@ -0,0 +1,3 @@
+export default function Error404Page() {
+  return <div>Pag not found 404</div>
+}
---
+++ src/pages/homepage/Homepage.tsx
@@ -0,0 +1,4 @@
+export default function Homepage() {
+  const x = 23
+  return <div>Homepage</div>
+}
