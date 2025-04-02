f24615f45f54fc151217c691d03beeef40acf2ba
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-04-02 19:00:46 +0600

    added logo

--- src/layout/AppLayoutHeader.tsx
+++ src/layout/AppLayoutHeader.tsx
@@ -6,7 +6,9 @@ import Username from '../components/ui/user/Username'
 export default function AppLayoutHeader() {
   return (
     <header className="row-auto flex items-center justify-between bg-yellow-500 uppercase border-b border-stone-200 px-3 py-4 sm:px-6">
-      <Link to="/">Pizzafy</Link>
+      <Link to="/" className="cursor-pointer">
+        <img className="h-8" src="/assets/images/logo.png" />
+      </Link>
       <OrderSearch />
       <Username />
     </header>
