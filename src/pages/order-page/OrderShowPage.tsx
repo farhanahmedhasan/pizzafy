aa1d3e9c5e0dca17373392f42cfe1520801cdb2a
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-04-08 01:40:39 +0600

    completed order page ui added date and calc min utility function and added order Item component

--- src/pages/order-page/OrderShowPage.tsx
+++ src/pages/order-page/OrderShowPage.tsx
@@ -1,6 +1,8 @@
 import { useLoaderData } from 'react-router'
 
+import { calcMinutesLeft, formatCurrency, formatDate } from '../../utils/helpers'
 import Pill from '../../components/ui/Pill'
+import OrderItem from './partials/OrderItem'
 
 export default function OrderShowPage() {
   const order = useLoaderData()
@@ -8,7 +10,7 @@ export default function OrderShowPage() {
 
   return (
     <section className="space-y-8 px-4 py-6">
-      <div className="flex flex-wrap gap-x-8 items-center justify-between">
+      <div className="flex flex-wrap gap-2 items-center justify-between">
         <h2 className="text-xl font-semibold">
           Order #{order.id}: {order.status}
         </h2>
@@ -18,7 +20,28 @@ export default function OrderShowPage() {
         </div>
       </div>
 
-      <div></div>
+      <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 px-6 py-5">
+        <p className="font-medium">
+          {calcMinutesLeft(order.estimatedDelivery) >= 0
+            ? `Only ${calcMinutesLeft(order.estimatedDelivery)} minutes left 😃`
+            : 'Order should have arrived'}
+        </p>
+        <p className="text-xs text-stone-500">(Estimated delivery: {formatDate(order.estimatedDelivery)})</p>
+      </div>
+
+      <ul className="divide-y divide-stone-200 border-y border-y-stone-200">
+        {order.cart.map((item) => (
+          <OrderItem key={item.pizzaId} item={item} />
+        ))}
+      </ul>
+
+      <div className="space-y-2 bg-stone-200 px-6 py-5">
+        <p className="text-sm font-medium text-stone-600">Price pizza: {formatCurrency(order.orderPrice)}</p>
+        {order.priority && (
+          <p className="text-sm font-medium text-stone-600">Price priority: {formatCurrency(order.priorityPrice)}</p>
+        )}
+        <p className="font-bold">To pay on delivery: {formatCurrency(order.orderPrice + order.priorityPrice)}</p>
+      </div>
     </section>
   )
 }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -13,6 +13,21 @@ function formatToSlug(value: string): string {
   return value.replace(/\s+/g, '-').trim()
 }
 
+export function formatDate(date: string) {
+  return new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'short',
+    hour: '2-digit',
+    minute: '2-digit'
+  }).format(new Date(date))
+}
+
+export function calcMinutesLeft(date: string) {
+  const d1 = new Date().getTime()
+  const d2 = new Date(date).getTime()
+  return Math.round((d2 - d1) / 60000)
+}
+
 function isValidPhone(value: string): boolean {
   return /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(value)
 }
