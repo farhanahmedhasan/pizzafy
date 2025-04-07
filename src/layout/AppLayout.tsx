01c0eba6fbaf85ec9cecbdc176f3b791b08c11f0
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-04-07 16:57:58 +0600

    added google fonts

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -7,7 +7,7 @@ import Spinner from './Spinner'
 export default function AppLayout() {
   const navigation = useNavigation()
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+    <div className="grid h-dvh grid-rows-[auto_1fr_auto]">
       <AppLayoutHeader />
 
       {navigation.state === 'loading' && <Spinner />}
