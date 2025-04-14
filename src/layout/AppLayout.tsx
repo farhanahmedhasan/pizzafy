20b758a18f9c26633e60edd4ca719c93f99dca9f
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-04-14 12:24:38 +0600

    only show cart overview when there is pizzas in cart

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet, useNavigation } from 'react-router'
+import { useSelector } from 'react-redux'
 
 import AppLayoutCartOverview from './AppLayoutCartOverview'
+import { getTotalPizzas } from '../features/cart/cartSlice'
 import AppLayoutHeader from './AppLayoutHeader'
 import Spinner from './Spinner'
 
 export default function AppLayout() {
   const navigation = useNavigation()
+  const totalPizzas = useSelector(getTotalPizzas)
+
   return (
     <div className="grid h-dvh grid-rows-[auto_1fr_auto]">
       <AppLayoutHeader />
@@ -15,7 +19,8 @@ export default function AppLayout() {
       <main className="overflow-y-auto">
         <Outlet />
       </main>
-      <AppLayoutCartOverview />
+
+      {totalPizzas > 0 && <AppLayoutCartOverview />}
     </div>
   )
 }
