668fff8d8d1f79f1d6805cdf3c358c9bcc6a0655
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-04-14 17:36:12 +0600

    fixed cart page when there is no item on the cart

--- src/pages/cart-page/CartPage.tsx
+++ src/pages/cart-page/CartPage.tsx
@@ -10,13 +10,15 @@ export default function CartPage() {
   const cart = useSelector(getCart)
   const dispatch = useDispatch()
 
+  const isCartEmpty = cart.length === 0
+
   return (
     <section className="px-4 py-3">
       <Button href="/menu" variant="link" size="none">
         &larr; Back to menu
       </Button>
 
-      <h2 className="text-xl font-semibold mt-7">Your cart, {username}</h2>
+      <h2 className="text-xl font-semibold mt-7">{isCartEmpty ? 'Your cart is empty' : `Your cart, ${username}`}</h2>
 
       <ul className="mt-2">
         {cart.map((item) => (
@@ -24,12 +26,15 @@ export default function CartPage() {
         ))}
       </ul>
 
-      <div className="mt-6 space-x-2">
-        <Button href="/order/new">Order Pizzas</Button>
-        <Button variant="secondary" className="py-2.5 sm:py-3.5" onClick={() => dispatch(resetCart())}>
-          Clear cart
-        </Button>
-      </div>
+      {isCartEmpty && <p className="font-semibold">Please add item to place an order</p>}
+      {!isCartEmpty && (
+        <div className="mt-6 space-x-2">
+          <Button href="/order/new">Order Pizzas</Button>
+          <Button variant="secondary" className="py-2.5 sm:py-3.5" onClick={() => dispatch(resetCart())}>
+            Clear cart
+          </Button>
+        </div>
+      )}
     </section>
   )
 }
